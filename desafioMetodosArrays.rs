2c4f8d5c43cc1b287de2ba3bd45d11e6c8aea33a
pichu2707 <pichu_2707@hotmail.com>
2026-06-25 00:54:46 +0200

    Subiendo ejercicios de rust

---
+++ desafioMetodosArrays.rs
@@ -0,0 +1,28 @@
+use std::io;
+use std::convert::TryInto;
+
+fn analyze_array(arr: &mut [String; 5]) {
+    // Write code here
+    // Vacío
+    let empty = arr.is_empty();
+    // Contiene
+    let has_five = arr.contains(&"5".to_string());
+    println!("Is empty: {}", empty);
+    println!("Contains 5: {}", has_five);
+    // Invierte
+    println!("Reversed Elements:");
+    arr.reverse();
+    
+
+    // Print elements line by line
+    for element in arr.iter() {
+        println!("{}", element);
+    }
+}
+
+fn main() {
+    let mut input_str_arr = String::new();
+    io::stdin().read_line(&mut input_str_arr).unwrap();
+    let mut arr: [String; 5] = input_str_arr.split(',').map(String::from).collect::<Vec<String>>().try_into().unwrap();
+    analyze_array(&mut arr);
+}
---
+++ desafioReferenciasArraysMutables.rs
@@ -0,0 +1,15 @@
+fn process_array(arr: &mut [i32]) {
+	arr[0] = 3;
+}
+
+fn main() {
+    let mut numbers = [1, 2, 3, 4, 5];
+    
+    // Call process_array
+    process_array(&mut numbers);
+
+    for i in 0..numbers.len() {
+        print!("{} ", numbers[i]);
+    }
+
+}
